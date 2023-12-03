5030be1ad4383b8aacb000ffd5a445d817466b76
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2023-12-03 17:00:09 +0100

    day 3 - end

--- bin/adventofcode.dart
+++ bin/adventofcode.dart
@@ -1,9 +1,9 @@
 import 'package:adventofcode/day1/day1.dart' as day1;
 import 'package:adventofcode/day2/day2.dart';
-import 'package:adventofcode/day3/day3.dart';
 
 void main(List<String> arguments) async {
+
   // day1.calcSumOfTextLine();
-  // day2();
-  day3();
+  day2();
+
 }
